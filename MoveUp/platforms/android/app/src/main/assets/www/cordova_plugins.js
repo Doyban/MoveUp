5711baaca57ec555a53bdce5f77555b904d046d6
Daniel Danielecki <daniel.danielecki@mail.com>
2022-10-26 11:55:05 +0200

    Add: Firebase configuration

--- MoveUp/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ MoveUp/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -45,6 +45,30 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "admob"
       ]
+    },
+    {
+      "id": "cordova-plugin-buildinfo.BuildInfo",
+      "file": "plugins/cordova-plugin-buildinfo/www/buildinfo.js",
+      "pluginId": "cordova-plugin-buildinfo",
+      "clobbers": [
+        "BuildInfo"
+      ]
+    },
+    {
+      "id": "cordova-plugin-browsertab.BrowserTab",
+      "file": "plugins/cordova-plugin-browsertab/www/browsertab.js",
+      "pluginId": "cordova-plugin-browsertab",
+      "clobbers": [
+        "cordova.plugins.browsertab"
+      ]
+    },
+    {
+      "id": "cordova-plugin-inappbrowser.inappbrowser",
+      "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+      "pluginId": "cordova-plugin-inappbrowser",
+      "clobbers": [
+        "cordova.InAppBrowser.open"
+      ]
     }
   ];
   module.exports.metadata = {
@@ -55,6 +79,9 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-plugin-purchase": "11.0.0",
     "cordova-plugin-whitelist": "1.3.5",
     "cordova-plugin-customurlscheme": "5.0.2",
-    "admob-plus-cordova": "1.28.0"
+    "admob-plus-cordova": "1.28.0",
+    "cordova-plugin-buildinfo": "4.0.0",
+    "cordova-plugin-browsertab": "0.2.0",
+    "cordova-plugin-inappbrowser": "5.0.0"
   };
 });
